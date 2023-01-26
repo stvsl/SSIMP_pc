54921c27177b7f88c782902e6fc52f4cb110f46e
stvsl <aptx4869stvsl@outlook.com>
2023-01-27 01:52:17 +0800

    弃用openssl,转而使用go编写动态链接库的形式完成

---
+++ Security/encryption.cpp
@@ -0,0 +1,164 @@
+#include "encryption.h"
+#include "Daemon/global.h"
+namespace AES {
+string encrypt(string str, string key) {
+  GoString str_ = {str.c_str(), (long int)str.length()};
+  GoString key_ = {key.c_str(), (long int)key.length()};
+  GoString ret_ = GoAESEncrypt(str_, key_);
+  string ret = string(ret_.p, ret_.n);
+#if ENCRYPT_ERR_DEBUG == 1
+  cout << endl
+       << "\033[1;33m"
+       << "**************RSA加密**************" << endl;
+  cout << "原文： " << str << endl;
+  cout << "密钥： " << key << endl;
+  cout << "密文： " << ret << endl;
+  cout << "**********************************" << endl << "\033[0m";
+#endif
+  return ret;
+}
+
+QString encryptQ(QString str, QString key) {
+  QByteArray strbyte = str.toUtf8();
+  QByteArray keybyte = key.toUtf8();
+  string str_ = strbyte.toStdString();
+  string key_ = keybyte.toStdString();
+  string ret_ = encrypt(str_, key_);
+  QString ret = QString::fromStdString(ret_);
+  return ret;
+}
+
+string encrypt(string str) {
+  QByteArray _key_ = global_Security::getAesKey().toUtf8();
+  string key_ = _key_.toStdString();
+  return encrypt(str, key_);
+}
+
+QString encryptQ(QString str) {
+  return encryptQ(str, global_Security::getAesKey());
+}
+
+string decrypt(string ciphertext, string key) {
+  GoString ciphertext_ = {ciphertext.c_str(), (long int)ciphertext.length()};
+  GoString key_ = {key.c_str(), (long int)key.length()};
+  GoString ret_ = GoAESDecrypt(ciphertext_, key_);
+  string ret = string(ret_.p, ret_.n);
+#if ENCRYPT_ERR_DEBUG == 1
+  cout << endl
+       << "\033[1;33m"
+       << "**************RSA解密**************" << endl;
+  cout << "密文： " << ciphertext << endl;
+  cout << "密钥： " << key << endl;
+  cout << "原文： " << ret << endl;
+  cout << "**********************************" << endl << "\033[0m";
+#endif
+  return ret;
+}
+
+QString decryptQ(QString ciphertext, QString key) {
+  QByteArray ciphertextbyte = ciphertext.toUtf8();
+  QByteArray keybyte = key.toUtf8();
+  string ciphertext_ = ciphertextbyte.toStdString();
+  string key_ = keybyte.toStdString();
+  string ret_ = decrypt(ciphertext_, key_);
+  QString ret = QString::fromStdString(ret_);
+  return ret;
+}
+
+string decrypt(string ciphertext) {
+  QByteArray _key_ = globalsecurity::AES_KEY.toUtf8();
+  string key_ = _key_.toStdString();
+  return decrypt(ciphertext, key_);
+}
+
+QString decryptQ(QString ciphertext) {
+  return decryptQ(ciphertext, globalsecurity::AES_KEY);
+}
+
+} // namespace AES
+
+namespace RSA {
+string encrypt(string str, string pubkey) {
+  GoString str_ = {str.c_str(), (long int)str.length()};
+  GoString pubkey_ = {pubkey.c_str(), (long int)pubkey.length()};
+  GoString ret_ = GoRSAEncrypt(str_, pubkey_);
+  string ret = string(ret_.p, ret_.n);
+#if ENCRYPT_ERR_DEBUG == 1
+  cout << endl
+       << "\033[1;33m"
+       << "**************RSA加密**************" << endl;
+  cout << "原文： " << str << endl;
+  cout << "密钥： " << pubkey << endl;
+  cout << "密文： " << ret << endl;
+  cout << "**********************************" << endl << "\033[0m";
+#endif
+  return ret;
+}
+
+QString encryptQ(QString str, QString pubkey) {
+  QByteArray strbyte = str.toUtf8();
+  QByteArray pubkeybyte = pubkey.toUtf8();
+  string str_ = strbyte.toStdString();
+  string pubkey_ = pubkeybyte.toStdString();
+  string ret_ = encrypt(str_, pubkey_);
+  QString ret = QString::fromStdString(ret_);
+  return ret;
+}
+
+string encrypt(string str) {
+  QByteArray _key_ = globalsecurity::LOCAL_RSA_PUBLIC.toUtf8();
+  string key_ = _key_.toStdString();
+  return encrypt(str, key_);
+}
+QString encryptQ(QString str) {
+  return encryptQ(str, globalsecurity::LOCAL_RSA_PUBLIC);
+}
+
+string decrypt(string str, string prikey) {
+  GoString str_ = {str.c_str(), (long int)str.length()};
+  GoString prikey_ = {prikey.c_str(), (long int)prikey.length()};
+  GoString ret_ = GoRSADecrypt(str_, prikey_);
+  string ret = string(ret_.p, ret_.n);
+#if ENCRYPT_ERR_DEBUG == 1
+  cout << endl
+       << "\033[1;33m"
+       << "**************RSA解密**************" << endl;
+  cout << "密文： " << str << endl;
+  cout << "密钥： " << prikey << endl;
+  cout << "原文： " << ret << endl;
+  cout << "**********************************" << endl << "\033[0m";
+#endif
+  return ret;
+}
+
+QString decryptQ(QString str, QString prikey) {
+  QByteArray strbyte = str.toUtf8();
+  QByteArray prikeybyte = prikey.toUtf8();
+  string str_ = strbyte.toStdString();
+  string prikey_ = prikeybyte.toStdString();
+  string ret_ = decrypt(str_, prikey_);
+  QString ret = QString::fromStdString(ret_);
+  return ret;
+}
+
+string decrypt(string str) {
+  QByteArray _key_ = globalsecurity::LOCAL_RSA_PRIVATE.toUtf8();
+  string key_ = _key_.toStdString();
+  return decrypt(str, key_);
+}
+
+QString decryptQ(QString str) { return decryptQ(str, globalsecurity::AES_KEY); }
+
+string generateRSAKey() {
+  GoString ret_ = GoRSAKey();
+  string ret = string(ret_.p, ret_.n);
+  return ret;
+}
+
+QString generateRSAKeyQ() {
+  GoString ret_ = GoRSAKey();
+  string stdret = string(ret_.p, ret_.n);
+  QString qret = QString::fromStdString(stdret);
+  return qret;
+}
+} // namespace RSA
--- Security/security.cpp
+++ Security/security.cpp
@@ -8,14 +8,10 @@
 
 #include "Daemon/global.h"
 #include "Utils/tcpnetutils.h"
-#include "rsa.h"
 #include <QCoreApplication>
 #include <QHostInfo>
 #include <QNetworkInterface>
 #include <Utils/npost.h>
-#include <openssl/evp.h>
-#include <openssl/pem.h>
-#include <openssl/rsa.h>
 #include <QObject>
 
 bool globalsecurity::inited = false;
@@ -33,33 +29,10 @@ bool Init_State_3();
  */
 bool global_Security::Init()
 {
-  // 创建本地RSA密钥对
-  EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new_from_name(NULL, "RSA", NULL);
-  EVP_PKEY_keygen_init(ctx);
-  EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, 2048);
-  EVP_PKEY *pkey = NULL;
-  EVP_PKEY_keygen(ctx, &pkey);
-  EVP_PKEY_CTX_free(ctx);
-  // 获取本地RSA私钥
-  BIO *pri = BIO_new(BIO_s_mem());
-  PEM_write_bio_PrivateKey(pri, pkey, NULL, NULL, 0, NULL, NULL);
-  char *pri_key;
-  long pri_len = BIO_get_mem_data(pri, &pri_key);
-  globalsecurity::LOCAL_RSA_PRIVATE = QString::fromLatin1(pri_key, pri_len);
-  BIO_free(pri);
-  // 获取本地RSA公钥
-  BIO *pub = BIO_new(BIO_s_mem());
-  PEM_write_bio_PUBKEY(pub, pkey);
-  char *pub_key;
-  long pub_len = BIO_get_mem_data(pub, &pub_key);
-  globalsecurity::LOCAL_RSA_PUBLIC = QString::fromLatin1(pub_key, pub_len);
-  BIO_free(pub);
-  // 释放EVP_PKEY
-  EVP_PKEY_free(pkey);
 
   qDebug() << "RSA密钥对生成成功";
-  // qDebug() << "本地RSA私钥：" << globalsecurity::LOCAL_RSA_PRIVATE;
-  // qDebug() << "本地RSA公钥：" << globalsecurity::LOCAL_RSA_PUBLIC;
+  qDebug().noquote() << "本地RSA私钥：" << globalsecurity::LOCAL_RSA_PRIVATE;
+  qDebug().noquote() << "本地RSA公钥：" << globalsecurity::LOCAL_RSA_PUBLIC;
 
   // 获取服务器RSA公钥
   TcpNetUtils *net = new TcpNetUtils(new TcpGet("/api/encryption/rsapubkey"));
@@ -160,19 +133,24 @@ bool Init_State_3()
   QJsonObject json;
   qDebug().noquote() << "服务器RSA公钥:\n"
                      << globalsecurity::SERVER_RSA_PUBLIC;
-  json.insert("feature", rsaPubEncrypt(globalsecurity::FEATURE,
-                                       globalsecurity::SERVER_RSA_PUBLIC));
-  json.insert("pubkey", rsaPubEncrypt(globalsecurity::LOCAL_RSA_PUBLIC,
-                                      globalsecurity::SERVER_RSA_PUBLIC));
-  json.insert("aesp", rsaPubEncrypt(globalsecurity::AES_KEY,
-                                    globalsecurity::SERVER_RSA_PUBLIC));
+  // json.insert("feature", rsaPubEncrypt(globalsecurity::FEATURE,
+  //                                      globalsecurity::SERVER_RSA_PUBLIC));
+  // json.insert("pubkey", rsaPubEncrypt(globalsecurity::LOCAL_RSA_PUBLIC,
+  //                                     globalsecurity::SERVER_RSA_PUBLIC));
+  // json.insert("aesp", rsaPubEncrypt(globalsecurity::AES_KEY,
+  //                                   globalsecurity::SERVER_RSA_PUBLIC));
   TcpPost *post = new TcpPost("/api/encryption/rsatoaes");
   post->setHeader("Content-Type", "application/json");
   post->setBody(json);
   TcpNetUtils *net = new TcpNetUtils(post);
   QObject::connect(net, &TcpNetUtils::requestFinished, [=]()
                    { globalsecurity::AES_KEY =
-                         net->getResponseBodyJsonDoc()["aeskey"].toString(); });
+                         net->getResponseBodyJsonDoc()["aesp2"].toString();
+                        //  globalsecurity::AES_KEY =
+                        //      rsaPriDecryptWithBase64(globalsecurity::AES_KEY,
+                        //                    globalsecurity::LOCAL_RSA_PRIVATE);
+                         qDebug().noquote() << "AES协议密钥:\n"
+                                            << globalsecurity::AES_KEY; });
   QObject::connect(net, &TcpNetUtils::requestErrorHappen, [=]()
                    {
                      // 连接vctrl静态类的信号
